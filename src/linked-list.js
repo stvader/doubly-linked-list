9b5bbcc2015c182a54b5424b0586987e8f58623a
st_Vader <st.vader.85@gmail.com>
2017-09-14 22:35:52 +0300

    make append, head, tail, at methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,15 +1,54 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = new Node();
+        this._tail = new Node();
+    }
 
-    append(data) {}
+    append(data) {
+        //let node = new Node(data);
 
-    head() {}
+       /* var node = {
+            value: value,
+            next: null,
+            prev: null,
+        }*/
+         
+        if (this.length === 0) {
+            let node = new Node(data);
+            this._head = node;//
+            this._tail = node;//
+        } else if (this.length === 1) {
+            this._tail = new Node(data, this._head);
+            this._head.next = this._tail;
+        } else {
+            let node = this._tail;
+            this._tail = new Node(data, node);
+            node.next = this._tail;            
+        }
 
-    tail() {}
+        this.length++;
+    }
 
-    at(index) {}
+    head() {
+        return this._head.data;
+    }
+
+    tail() {
+        //if (this.length === 1) return this._head;
+        return this._tail.data;
+    }
+
+    at(index) {
+        let node = this._head;
+        for (let i=0; i<index; i++) {
+            node = node.next;             
+        }
+
+        return node.data;
+    }
 
     insertAt(index, data) {}
 
